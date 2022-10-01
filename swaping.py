4d44d3e14d5908381867fc52ab9076321121fca0
Will <113240809+Anti1212@users.noreply.github.com>
2022-10-01 05:57:14 +0530

    Update swaping.py

--- swaping.py
+++ swaping.py
@@ -1,12 +1,9 @@
-fis=input("enter the value of first veriable: ")
-sec=input("enter the value of second veriable: ")
+fis=input("Enter the value of first veriable: ")
+sec=input("Enter the value of second veriable: ")
 
-print("value of first veriable is ",fis)
-print("value of second veriable is ",sec)
-
-print("swaping the first and second veriables")
+print("Swaping the first and second veriables")
 
 fis,sec=sec,fis
-print("---after swaping---")
+print("-------after swaping-------")
 print("value of first veriable is ",fis)
 print("value of second veriable is ",sec)
